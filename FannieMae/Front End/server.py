c35866728fabd8b26db5aaeb4eba7d8fff2184cf
LastBrainCells <sahilshethdevelopment@gmail.com>
2023-11-04 22:21:24 -0500

    Added Input forms to work with Back End Calculations

--- FannieMae/Front End/server.py
+++ FannieMae/Front End/server.py
@@ -8,16 +8,35 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    gmi = request.form.get('gmi')
-    ccp = request.form.get('ccp')
-    cp = request.form.get('cp')
-    slp = request.form.get('slp')
-    av = request.form.get('av')
-    dp = request.form.get('dp')
-    la = request.form.get('la')
-    mmp = request.form.get('mmp')
-    cs = request.form.get('cs')
-    return f'You entered: {gmi}, {ccp}, {cp}, {slp}, {av}, {dp}, {la}, {mmp}, {cs}'
+    gmi = float(request.form.get('gmi'))
+    ccp = float(request.form.get('ccp'))
+    cp = float(request.form.get('cp'))
+    slp = float(request.form.get('slp'))
+    av = float(request.form.get('av'))
+    dp = float(request.form.get('dp'))
+    la = float(request.form.get('la'))
+    mmp = float(request.form.get('mmp'))
+    cs = int(request.form.get('cs'))
+
+    goodCredit = "Good"
+
+    if cs < 640:
+        goodCredit = "Bad"
+
+    ltv = round((av - dp) / av, 2)
+
+    pmiRate = 1.01
+
+    if ltv > 0.8:
+        av = round(av * 1.01, 2)
+
+    monthlyDebt = round(ccp + cp + mmp, 2)
+
+    dti = round(monthlyDebt / gmi, 2)
+
+    fedti = round(mmp / gmi, 2)
+
+    return render_template('results.html', gmi=f"{gmi:.2f}", ccp=f"{ccp:.2f}", cp=f"{cp:.2f}", slp=f"{slp:.2f}", av=f"{av:.2f}", dp=f"{dp:.2f}", la=f"{la:.2f}", mmp=f"{mmp:.2f}", cs=f"{cs:.2f}", goodCredit=goodCredit, pmiRate=f"{pmiRate:.2f}", ltv=f"{ltv:.2f}", monthlyDebt=f"{monthlyDebt:.2f}", dti=f"{dti:.2f}", fedti=f"{fedti:.2f}")
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
